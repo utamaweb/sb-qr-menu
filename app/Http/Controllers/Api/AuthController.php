65a2e5e46a91b468419ccab9bc576f92d2ef5285
Juliadwiagustina <argifatur@gmail.com>
2024-05-06 13:53:19 +0800

    update auth api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
             $token = JWTAuth::attempt($credentials);
 
             if (!$token) {
-                return response()->json(['message' => 'Login credentials are invalid'], 200);
+                return response()->json(['message' => 'Login credentials are invalid'], 400);
             }
 
             $user = getUser($request->email);

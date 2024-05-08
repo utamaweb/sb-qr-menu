5afa9a89df413361e6b90938a54c13e3c7d1535a
syafiqmk <syafiqmuh.k@gmail.com>
2024-05-08 09:46:20 +0800

    Fix migration bug add product_warehouse migration

---
+++ database/migrations/2024_05_08_094231_product_warehouse_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_warehouse', function (Blueprint $table) {
+            $table->increments('id');
+            // $table->unsignedBigInteger('product_id');
+            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products', 'id')->onDelete('cascade');
+            // $table->unsignedBigInteger('warehouse_id');
+            // $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade');
+            $table->foreignId('warehouse_id')->constrained('warehouses', 'id')->onDelete('cascade');
+            $table->bigInteger('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_warehouse');
+    }
+};

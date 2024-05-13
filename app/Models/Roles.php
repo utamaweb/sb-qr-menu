4af453d3139d5c85a28fb50f80b7058f84de677b
argifatur <argifatur@gmail.com>
2024-05-13 16:25:34 +0800

    update bug user

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Roles extends Model
 {
-    use SoftDeletes;
     protected $guard_name = 'web';
     protected $fillable =[
         "name", "description", "guard_name", "is_active"

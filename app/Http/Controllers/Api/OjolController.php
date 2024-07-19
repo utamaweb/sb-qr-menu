836b96658b4b657383472cd8bd9676c134a07ded
syafiqmk <syafiqmuh.k@gmail.com>
2024-07-19 22:57:01 +0800

    Update Ojols API response to add offline option for cashier only outlet type

--- app/Http/Controllers/Api/OjolController.php
+++ app/Http/Controllers/Api/OjolController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Ojol;
 use App\Models\Warehouse;
+use App\Models\OrderType;
 use Illuminate\Http\Request;
 use App\Models\OjolWarehouse;
 
@@ -26,7 +27,23 @@ class OjolController extends Controller
             } else {
                 continue;
             }
+
+            $ojol['type'] = "online";
         }
+
+        // Check outlet service type
+        $serviceType = auth()->user()->warehouse->is_self_service;
+        if($serviceType == 0) {
+            // Get order types
+            $orderTypes = OrderType::all();
+
+            // Loop to push order types to ojols
+            foreach($orderTypes as $item) {
+                $item['type'] = "offline";
+                $ojols->push($item);
+            }
+        }
+
         return response()->json($ojols, 200);
     }
 }

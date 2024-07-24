3e490b524556253c3f450a7b6885ff73681f63bc
syafiqmk <syafiqmuh.k@gmail.com>
2024-07-24 10:49:15 +0800

    Fix custom category datatable bug

--- resources/views/backend/custom_category/index.blade.php
+++ resources/views/backend/custom_category/index.blade.php
@@ -47,3 +47,73 @@
 </section>
 
 @endsection
+
+@push('scripts')
+<script>
+    $('#category-table').DataTable({
+            "order": [],
+            'language': {
+                'lengthMenu': '_MENU_ {{ trans('file.records per page') }}',
+                "info": '<small>{{ trans('file.Showing') }} _START_ - _END_ (_TOTAL_)</small>',
+                "search": 'Cari',
+                'paginate': {
+                    'previous': '<i class="dripicons-chevron-left"></i>',
+                    'next': '<i class="dripicons-chevron-right"></i>'
+                }
+            },
+            'columnDefs': [
+                // {
+                //     "orderable": false,
+                //     'targets': [0, 2]
+                // },
+            ],
+            'select': {
+                style: 'multi',
+                selector: 'td:first-child'
+            },
+            'lengthMenu': [
+                [10, 25, 50, -1],
+                [10, 25, 50, "All"]
+            ],
+            dom: '<"row"lfB>rtip',
+            buttons: [{
+                    extend: 'pdf',
+                    text: '<i title="export to pdf" class="fa fa-file-pdf-o"></i>',
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                },
+                {
+                    extend: 'excel',
+                    text: '<i title="export to excel" class="dripicons-document-new"></i>',
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                },
+                {
+                    extend: 'csv',
+                    text: '<i title="export to csv" class="fa fa-file-text-o"></i>',
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                },
+                {
+                    extend: 'print',
+                    text: '<i title="print" class="fa fa-print"></i>',
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                },
+                {
+                    extend: 'colvis',
+                    text: '<i title="column visibility" class="fa fa-eye"></i>',
+                    columns: ':gt(0)'
+                },
+            ],
+        });
+</script>
+@endpush

5b57487865b4f3682bf403a7e0f312bd7996b04c
argifatur <argifatur@gmail.com>
2024-11-11 09:50:41 +0800

    add log close cashier

--- app/Http/Controllers/Api/ShiftController.php
+++ app/Http/Controllers/Api/ShiftController.php
@@ -447,6 +447,7 @@ class ShiftController extends Controller
             return response()->json($response, 200);
         } catch (\Throwable $th) {
             DB::rollback();
+            \Log::emergency("File:" . $th->getFile() . " Line:" . $th->getLine() . " Message:" . $th->getMessage());
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }

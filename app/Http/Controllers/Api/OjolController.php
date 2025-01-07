2a230abf9d82164add42b1f0a28c1c44aab498d7
Syafiq Muhammad Kahfi <syafiqmuh.k@gmail.com>
2025-01-07 16:15:48 +0800

    API Ojek Online: Update index

--- app/Http/Controllers/Api/OjolController.php
+++ app/Http/Controllers/Api/OjolController.php
@@ -13,16 +13,13 @@ class OjolController extends Controller
 {
     public function index()
     {
-        $business_id = Warehouse::find(auth()->user()->warehouse_id)->business_id;
-        $ojols = Ojol::where('business_id', $business_id)->get();
-        $ojols = Ojol::where('business_id', '=', $business_id)->where('deleted_at', '=', NULL)->get();
+        $ojols = Ojol::where('business_id', '=', auth()->user()->warehouse->business_id)->where('deleted_at', '=', NULL)->get();
 
         foreach ($ojols as $ojol) {
-            $ojolWarehouseCheck = OjolWarehouse::where('warehouse_id', '=', auth()->user()->warehouse_id)->where('ojol_id', '=', $ojol->id)->exists();
             $ojolWarehouse = OjolWarehouse::where('warehouse_id', '=', auth()->user()->warehouse_id)->where('ojol_id', '=', $ojol->id)->first();
 
-            if ($ojolWarehouseCheck) {
-                $ojol->percent = $ojolWarehouse->percent;
+            if ($ojolWarehouse != null) {
+                $ojol->percent     = $ojolWarehouse->percent;
                 $ojol->extra_price = $ojolWarehouse->extra_price;
             } else {
                 continue;

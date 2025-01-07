b1e334d7ffb6a33be6141654033a1ff0d9323aff
Syafiq Muhammad Kahfi <syafiqmuh.k@gmail.com>
2025-01-07 16:18:27 +0800

    API Check Service: Update

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -10,8 +10,8 @@ class ServiceController extends Controller
     // Check Self Service Status
     public function checkService() {
         return response()->json([
-            'status' => 'Ok',
-            'warehouse' => auth()->user()->warehouse->name,
+            'status'          => 'Ok',
+            'warehouse'       => auth()->user()->warehouse->name,
             'is_self_service' => (auth()->user()->warehouse->is_self_service == 1) ? true : false
         ], 200);
     }

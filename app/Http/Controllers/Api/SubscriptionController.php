2c1a959a4f80676b4a4832d2492017e5f74c35ed
argifatur <argifatur@gmail.com>
2025-02-24 12:13:13 +0800

    init status string value

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -15,7 +15,7 @@ class SubscriptionController extends Controller
     public function getStatus() {
         $outlet = Warehouse::find(auth()->user()->warehouse_id);
 
-        $status = '';
+        $status = 'Belum ada tanggal expired.';
         $isExpired = false;
         $message = '';
 
